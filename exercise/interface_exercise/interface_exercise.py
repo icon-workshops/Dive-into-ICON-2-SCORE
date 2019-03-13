6f96e25815b6b91f86513a4a07f85aab9c318504
Life4honor <choejy0329@gmail.com>
2019-03-13 23:41:58 +0900

    Update SCORE samples for exercise

--- exercise/interface_exercise/interface_exercise.py
+++ exercise/interface_exercise/interface_exercise.py
@@ -30,9 +30,24 @@ class InterfaceExercise(IconScoreBase):
     def on_update(self) -> None:
         super().on_update()
 
+    @eventlog(indexed=2)
+    def ModifyScoreStatus(self, _scoreName, _introduction):
+        pass
+
+    @eventlog
+    def IcxReceived(self, _from, _amount):
+        pass
+
     @property
     def time_recording(self):
         return ArrayDB("TIME_RECORDING", self._db, value_type=int)
+
+    @property
+    def score_status(self):
+        score_name = self.status['SCORE_NAME']
+        introduction = self.status['INTRODUCTION']
+        apis = self.status['APIS']
+        return "SCORE_NAME : " + score_name + "\n" + "INTRODUCTION : " + introduction + "\n" + "APIS : " + apis
 
     @external(readonly=True)
     def interfaceScoreExercise(self) -> str:
@@ -44,13 +59,6 @@ class InterfaceExercise(IconScoreBase):
     def getScoreStatus(self) -> str:
         return self.score_status
 
-    @property
-    def score_status(self):
-        score_name = self.status['SCORE_NAME']
-        introduction = self.status['INTRODUCTION']
-        apis = self.status['APIS']
-        return "SCORE_NAME : " + score_name + "\n" + "INTRODUCTION : " + introduction + "\n" + "APIS : " + apis
-
     @external(readonly=True)
     def getTimeRecording(self) -> str:
         time_recorded = [time_recorded for time_recorded in self.time_recording]
@@ -58,6 +66,7 @@ class InterfaceExercise(IconScoreBase):
 
     @external
     def modifyScoreStatus(self, _scoreName: str, _introduction: str):
+        self.ModifyScoreStatus()
         self.status["SCORE_NAME"] = _scoreName
         self.status["INTRODUCTION"] = _introduction
         self.status["APIS"] = str(self.get_api())
@@ -65,5 +74,25 @@ class InterfaceExercise(IconScoreBase):
 
     def put_time_recording(self):
         self.time_recording.put(self.block.height)
+
+    @payable
+    @external
+    def icxSend(self):
+        if not self.icx.send(self.msg.sender, self.msg.value):
+            revert(f"Resending ICX to msg.sender has failed with self.icx.send")
+
+
+    @payable
+    @external
+    def icxTransfer(self):
+        try:
+            self.icx.transfer(self.msg.sender, self.msg.value)
+        except Exception as e:
+            revert(f"{e} occurred while resending ICX with self.icx.transfer")
+
+    @payable
+    def fallback(self):
+        self.IcxReceived(self.msg.sender, self.msg.value)
+        pass
 
 
